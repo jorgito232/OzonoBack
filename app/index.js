5193dfd898a47aa065dc7b13938c8984391b37ff
jorge gaviria <jorge.gaviria@quipux.com>
2019-04-04 08:56:04 -0500

    add methods index.js

---
+++ app/index.js
@@ -0,0 +1,32 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { importSchema } = require('graphql-import')
+const { makeExecutableSchema } = require('graphql-tools') 
+const db = require('./utils/DataBase')
+const typeDefs = importSchema('./schema.graphql')
+const resolvers = require('./resolvers')
+
+const PORT = process.env.PORT 
+
+const connection = db.connectDB()
+
+connection.then(() => {
+  console.log('DB Connected')
+}).catch((err) => {
+  console.log(err)
+})
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+const options = {
+	port: process.env.PORT || 8000,
+	endpoint: "/graphql",
+	playground: "/playground",
+	cors:{
+		origin:"*"
+	}
+};
+
+const server = new GraphQLServer({schema})
+
+server.start(options, (_, () => console.log('Server Connected')))
+
